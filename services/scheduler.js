4883279ab87f01167f4c96b01dbfe38ef4dfeb36
Saiful <azadahmedofficial@gmail.com>
2025-08-12 10:47:59 +0600

    cron time change

--- services/scheduler.js
+++ services/scheduler.js
@@ -4,7 +4,7 @@ import { generateAndSavePosts } from "../utils/postGenerator.js";
 
 export function startScheduler() {
   cron.schedule(
-    "45 10 * * *", // Runs every day at 11:40 AM Dhaka time
+    "57 10 * * *", // Runs every day at 11:40 AM Dhaka time
     () => {
       console.log("🕘 Running scheduled job at 9:00 AM Dhaka time");
       generateAndSavePosts();
